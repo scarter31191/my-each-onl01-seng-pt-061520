92681c02cfb9ca0aaa45d4876e43b5d0fa5e8f5c
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-26 18:06:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+def name(list)
+  i = 0
+  names = []
+  while i < list.length
+    names << yield(array[i])
+    i += 1
+  end
+  names
+end
+
+
+
